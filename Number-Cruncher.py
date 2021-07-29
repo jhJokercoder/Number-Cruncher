b8a725ac6699d7ca05b7e7cb653baf536ac28d05
Jaydan Herrera <88108985+jhJokercoder@users.noreply.github.com>
2021-07-28 19:55:55 -0700

    Included Error Handling

--- Number-Cruncher.py
+++ Number-Cruncher.py
@@ -1,51 +1,99 @@
 #Number Cruncher
 import random
-import turtle
 
-def playgame(number, text, answer, x, y, othertext):
-    number = int(input(text))
+
+
+def playgame(x,y):
+    hints = ["Number is too high.", "Number is too low."]
+    hint_counter = 0
+    print(answer)
     answer = random.randint(x,y)
-    while 1<=number1<=10 and number1 != answer:
-        number1= int(input("Guess Again! Pick a number between x and y, inclusive."))
-        if 1<=number1<=10 and number1 == answer:
-            print("Congratulations! You guessed the correct number and have won the game!")
-            draw_winner()
+
     
 
-def draw_winner():
-    screen = turtle.Screen()
-    screen.bgcolor("white")
-    a = turtle.Turtle()
-    a.forward(100)
-    screen.exitonclick()
+    while True:
+        try:
+            number = int(input("Pick a number between {} and {}, inclusive: ".format(x,y)))
+        
+        except ValueError:
+            print("Must input an integer value!")
+            continue
+        else:
+            break
+    while x <= number <= y and number != answer:
+        
+        if number > answer:
+            
+            print(hints[0])
+            hint_counter += 1
+        elif number < answer:
+            print(hints[1])
+            hint_counter += 1
+        print(f"Hint Count = {hint_counter}")
+        if hint_counter > 5:
+            print("Game Over!")
+            break    
     
+        while True:
+            try:
+                number = int(input("Guess Again! Pick a number between {} and {}, inclusive: ".format(x,y)))
+        
+            except ValueError:
+                print("Must input an integer value!")
+                continue
+            else:
+                break
+    while x <= number <= y and number == answer:
+        print("Congratulations! You have guessed the correct integer and have won the game!")
+        break
+    while number < x or number > y:
+        while True:
+            try:
+                number = int(input("Must pick an integer within specified range. Pick a number between {} and {}, inclusive: ".format(x,y)))
+            except ValueError:
+                print("Must input an integer value!")
+                continue
+            else:
+                break
+        while x <= number <= y and number != answer:
+        
+            if number > answer:
+            
+                print(hints[0])
+                hint_counter += 1
+            elif number < answer:
+                print(hints[1])
+                hint_counter += 1
+            print(f"Hint Count = {hint_counter}")
+        
+            if hint_counter > 5:
+                print("Game Over!")
+                break
+            while True:
+                try:
+                    number = int(input("Guess Again! Pick a number between {} and {}, inclusive: ".format(x,y)))
+                except ValueError:
+                    print("Must input an integer value!")
+                    continue
+                else:
+                    break
+        while x <= number <= y and number == answer:
+            print("Congratulations! You have guessed the correct integer and have won the game!")
+            break
+        
+                
+
 def easy():
-    number1 = int(input("Pick a number between 1 and 10, inclusive."))
-    answer = random.randint(1,10)
-    while 1<=number1<=10 and number1 != answer:
-        number1= int(input("Guess Again! Pick a number between 1 and 10, inclusive."))
-        if 1<=number1<=10 and number1 == answer:
-            print("Congratulations! You guessed the correct number and have won the game!")
-            draw_winner()
+    playgame(1,10)
         
 def medium():
-    number2 = int(input("Pick a number between 1 and 100, inclusive."))
-    answer = random.randint(1,100)
-    while 1<=number2<=100 and number2 != answer:
-        number2= int(input("Guess Again! Pick a number between 1 and 100, inclusive."))
-        if 1<=number2<=100 and number2 == answer:
-            print("Congratulations! You guessed the correct number and have won the game!")
-            draw_winner()
-
+    playgame(1,100)
+    
 def hard():
-    number3 = int(input("Pick a number between 1 and 1000, inclusive."))
-    answer = random.randint(1,1000)
-    while 1<=number3<=1000 and number3 != answer:
-        number3= int(input("Guess Again! Pick a number between 1 and 1000, inclusive."))
-        if 1<=number3<=1000 and number3 == answer:
-            print("Congratulations! You guessed the correct number and have won the game!")
-            draw_winner()
+    playgame(1,1000)
 
+
+    
 difficulty_levels = {"Easy": "1-10", "Medium": "1-100", "Hard" : "1-1000"}
 for key in difficulty_levels:
     print(key + ":" + " " + difficulty_levels[key])
@@ -63,14 +111,13 @@ elif x == "Hard":
 
 
 while x not in choose_level:
-        print("Typo Error. You must type 'Easy', 'Medium', or 'Hard' to choose a level.")
-        x = input("Choose a level {}:".format(choose_level) + " ")
-        print("You chose: {}".format(x))
-        if x == "Easy":
-            easy()
-        elif x == "Medium":
-            medium()
-        elif x == "Hard":
-            hard()
-
+    print("Typo Error. You must type 'Easy', 'Medium', or 'Hard' to choose a level.")
+    x = input("Choose a level {}:".format(choose_level) + " ")
+    print("You chose: {}".format(x))
+    if x == "Easy":
+        easy()
+    elif x == "Medium":
+        medium()
+    elif x == "Hard":
+        hard()
 
