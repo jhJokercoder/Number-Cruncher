8c08af2e900e28dfea03dff505140c3e2b3d3f8d
Jaydan Herrera <88108985+jhJokercoder@users.noreply.github.com>
2021-07-29 14:57:36 -0700

    Added Welcome Message, Added Turtle Graphic, Added "Answer Was" Message

--- Number-Cruncher.py
+++ Number-Cruncher.py
@@ -1,15 +1,20 @@
-#Number Cruncher
+# Number Cruncher
 import random
+import turtle
 
 
-
+def drawend():
+    window = turtle.Screen()
+    window.bgcolor('black')
+    text = turtle.Turtle()
+    style = ('Times New Roman', 100, 'bold')
+    text.color('red')
+    text.write("Game Over", font=style, align='center')
+    text.hideturtle()
+    window.exitonclick()
 def playgame(x,y,answer):
     hints = ["Number is too high.", "Number is too low."]
     hint_counter = 0
-    print(answer)
-
-    
-
     while True:
         try:
             number = int(input("Pick a number between {} and {}, inclusive: ".format(x,y)))
@@ -19,10 +24,9 @@ def playgame(x,y,answer):
             continue
         else:
             break
-    while x <= number <= y and number != answer:
         
+    while x <= number <= y and number != answer:
         if number > answer:
-            
             print(hints[0])
             hint_counter += 1
         elif number < answer:
@@ -30,12 +34,13 @@ def playgame(x,y,answer):
             hint_counter += 1
         print(f"Hint Count = {hint_counter}")
         if hint_counter > 5:
+            print(f"Answer was: {answer}")
             print("Game Over!")
+            drawend()
             break    
-    
         while True:
             try:
-                number = int(input("Guess Again! Pick a number between {} and {}, inclusive: ".format(x,y)))
+                number = int(input("Guess Again! \nPick a number between {} and {}, inclusive: ".format(x,y)))
         
             except ValueError:
                 print("Must input an integer value!")
@@ -43,41 +48,40 @@ def playgame(x,y,answer):
             else:
                 break
     while x <= number <= y and number == answer:
-        print("Congratulations! You have guessed the correct integer and have won the game!")
+        print("Congratulations! \nYou have guessed the correct integer and have won the game!")
         break
     while number < x or number > y:
         while True:
             try:
-                number = int(input("Must pick an integer within specified range. Pick a number between {} and {}, inclusive: ".format(x,y)))
+                number = int(input("Must pick an integer within specified range. \nPick a number between {} and {}, inclusive: ".format(x,y)))
             except ValueError:
                 print("Must input an integer value!")
                 continue
             else:
                 break
         while x <= number <= y and number != answer:
-        
             if number > answer:
-            
                 print(hints[0])
                 hint_counter += 1
             elif number < answer:
                 print(hints[1])
                 hint_counter += 1
             print(f"Hint Count = {hint_counter}")
-        
             if hint_counter > 5:
+                print(f"Answer was: {answer}")
                 print("Game Over!")
+                drawend()
                 break
             while True:
                 try:
-                    number = int(input("Guess Again! Pick a number between {} and {}, inclusive: ".format(x,y)))
+                    number = int(input("Guess Again! \nPick a number between {} and {}, inclusive: ".format(x,y)))
                 except ValueError:
                     print("Must input an integer value!")
                     continue
                 else:
                     break
         while x <= number <= y and number == answer:
-            print("Congratulations! You have guessed the correct integer and have won the game!")
+            print("Congratulations! \nYou have guessed the correct integer and have won the game!")
             break
         
                 
@@ -92,7 +96,9 @@ def hard():
     playgame(1,1000,random.randint(1,1000))
 
 
-    
+print("Welcome to Number-Cruncher!")
+print("Objective: \nGuess the correct integer for the chosen range within 5 hints.")
+print("\u0332".join("Levels "))
 difficulty_levels = {"Easy": "1-10", "Medium": "1-100", "Hard" : "1-1000"}
 for key in difficulty_levels:
     print(key + ":" + " " + difficulty_levels[key])
@@ -110,7 +116,7 @@ elif x == "Hard":
 
 
 while x not in choose_level:
-    print("Typo Error. You must type 'Easy', 'Medium', or 'Hard' to choose a level.")
+    print("Typo Error! \nYou must type 'Easy', 'Medium', or 'Hard' to choose a level.")
     x = input("Choose a level {}:".format(choose_level) + " ")
     print("You chose: {}".format(x))
     if x == "Easy":
